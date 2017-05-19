a610f94b490328d12b65e6f494ce18600f79375b
Max <azievmaksim@gmail.com>
2017-05-19 18:05:04 +0300

    Задание $12 - работа со строкой запроса

---
+++ templates/leftBar.php
@@ -0,0 +1,29 @@
+<section class="content__side">
+    <h2 class="content__side-heading">Проекты</h2>
+
+    <nav class="main-navigation">
+        <ul class="main-navigation__list">
+
+            <?php
+
+            foreach ($project_list as $alias => $project_Name):
+                $css_class_active = '';
+                $index = 0;
+                $url = "index.php?projectget=$alias";
+
+                /*if ($index == 0) {
+                    $css_class_active = " main-navigation__list-item--active";
+                } */
+                ?>
+                <li class="main-navigation__list-item<?= $css_class_active; ?>">
+                    <a class="main-navigation__list-item-link" href="/<?= $url; ?>"> <?= $project_Name; ?></a>
+                    <span class="main-navigation__list-item-count"> <?= amountTaskInProject($task_list, $project_Name); ?> </span>
+                </li>
+            <?php endforeach; ?>
+
+        </ul>
+    </nav>
+
+    <a class="button button--transparent button--plus content__side-button" href="#">Добавить проект</a>
+</section>
+
